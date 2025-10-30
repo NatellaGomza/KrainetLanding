c00f1fb4fd551877d30e7e202b32b10745f02218
G_GomzaNR <gomza.natella@gmail.com>
2025-10-30 21:05:10 +0300

    first block

---
+++ app/script.js
@@ -0,0 +1,14 @@
+const burger = document.querySelector('.nav__burger');
+const navList = document.querySelector('.nav__list');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navList.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+    if (!navList.contains(e.target) && !burger.contains(e.target)) {
+        burger.classList.toggle('active');
+        navList.classList.toggle('active');
+    }
+});
