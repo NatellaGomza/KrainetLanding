0cde0bc5d15b17287cda32f29bdea0efdaca12b3
G_GomzaNR <gomza.natella@gmail.com>
2025-10-31 23:03:22 +0300

    first block fix, add second block

--- app/script.js
+++ app/script.js
@@ -12,3 +12,35 @@ document.addEventListener('click', (e) => {
         navList.classList.toggle('active');
     }
 });
+
+const programs = [
+    {
+        title: "Похудение",
+        subtitle: "Ваш кот весит больше собаки и почти утратил способность лазить по деревьям? Пора на диету! Cat Energy Slim поможет вашему питомцу сбросить лишний вес.",
+        image: "assets/cat_back.png",
+        button: "Каталог slim"
+    },
+    {
+        title: "Набор массы",
+        subtitle: "Заработать авторитет среди дворовых котов и даже собак? Серия Cat Energy Pro поможет вашему коту нарастить необходимые мышцы!",
+        image: "assets/cat_back2.png",
+        button: "Каталог pro"
+    }
+];
+
+
+const section = document.getElementById("programs");
+
+programs.map((item) => {
+    const card = document.createElement("div");
+    card.className = "main-programs__item";
+
+    card.innerHTML = `
+    <img class="main-programs__item-image" src="${item.image}" alt="" />
+    <h1 class="main-programs__item-title">${item.title}</h1>
+    <p class="main-programs__item-content">${item.subtitle}</p>
+    <div class="main-programs__item-button">${item.button}</div>
+  `;
+
+    section.appendChild(card);
+});
