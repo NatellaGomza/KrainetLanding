a9f6a7585bbe840e2f23b1ab13ea8b223d119bf4
G_GomzaNR <gomza.natella@gmail.com>
2025-11-04 23:37:47 +0300

    add responsiveness to footer

--- app/js/components/footer.js
+++ app/js/components/footer.js
@@ -1,14 +1,27 @@
 import {logo} from '../svg/logo.js';
+import {iconHTMLAcademy} from '../svg/footer/iconHTMLAcademy.js';
+import {iconInst} from '../svg/footer/iconInst.js';
+import {iconFacebook} from '../svg/footer/iconFB.js';
+import {iconVk} from '../svg/footer/iconVk.js';
 export function renderFooter() {
+    const icons=[iconVk, iconInst, iconFacebook];
     const footerSection = document.getElementById("footer");
 
-   footerSection.innerHTML = ` <div class="main-footer__logo">
- ${logo()}
-        </div>
-        <div class="main-footer__icons"></div>
-        <div class="main-footer__html-academy">
-            <span>HTML Academy</span>
+    const iconsHTML = icons.map(el => `<span>${el()}</span>`).join("");
 
-        </div>`
+    footerSection.innerHTML = `
+    <div class="main-footer__logo">
+      ${logo()}
+    </div>
+    <div class="main-footer__divider"></div>
+    <div class="main-footer__icons">
+      ${iconsHTML}
+    </div>
+    <div class="main-footer__divider"></div>
+    <div class="main-footer__html-academy">
+      <span>HTML Academy</span>
+      <span>${iconHTMLAcademy()}</span>
+    </div>
+  `
 }
 
