5a7f14c7f716d0fa990fa579353f2f73c3ceb16b
G_GomzaNR <gomza.natella@gmail.com>
2025-11-05 22:10:39 +0300

    fix ymaps

--- app/js/components/address.js
+++ app/js/components/address.js
@@ -17,7 +17,7 @@ export function renderMap() {
       { hintContent: 'Моя точка' },
       {
         iconLayout: 'default#image',
-        iconImageHref: './../../app/assets/map-logo.png',
+        iconImageHref: './app/assets/map-logo.png',
         iconImageSize: [48, 48],
         iconImageOffset: [-24, -48],
       }
