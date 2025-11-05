fd4169b0bfa2ef39285dc83cc4f774793f9e4764
G_GomzaNR <gomza.natella@gmail.com>
2025-11-05 23:23:00 +0300

    fix burger, add links

--- app/js/components/burgerMenu.js
+++ app/js/components/burgerMenu.js
@@ -1,11 +1,12 @@
 export function initBurgerMenu() {
-  const burger = document.querySelector('.nav__burger');
-  const navList = document.querySelector('.nav__list');
+    const burger = document.querySelector('.nav__burger');
+    const navList = document.querySelector('.nav__list');
+    const navLinks = navList.querySelectorAll('a');
 
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    navList.classList.toggle('active');
-  });
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        navList.classList.toggle('active');
+    });
 
     document.addEventListener('click', (e) => {
         const isClickInsideMenu = navList.contains(e.target) || burger.contains(e.target);
@@ -15,4 +16,13 @@ export function initBurgerMenu() {
             navList.classList.remove('active');
         }
     });
+
+    // 👇 Закрытие меню при клике на ссылку
+    navLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            burger.classList.remove('active');
+            navList.classList.remove('active');
+        });
+    });
 }
+
--- app/js/components/footer.js
+++ app/js/components/footer.js
@@ -1,15 +1,29 @@
-import { logo } from '../svg/logo.js';
-import { iconHTMLAcademy } from '../svg/footer/iconHTMLAcademy.js';
-import { iconInst } from '../svg/footer/iconInst.js';
-import { iconFacebook } from '../svg/footer/iconFB.js';
-import { iconVk } from '../svg/footer/iconVk.js';
+import {logo} from '../svg/logo.js';
+import {iconHTMLAcademy} from '../svg/footer/iconHTMLAcademy.js';
+import {iconInst} from '../svg/footer/iconInst.js';
+import {iconFacebook} from '../svg/footer/iconFB.js';
+import {iconVk} from '../svg/footer/iconVk.js';
+
 export function renderFooter() {
-  const icons = [iconVk, iconInst, iconFacebook];
-  const footerSection = document.getElementById('footerWrapper');
+    const icons = [
+        {
+            link: 'https://vk.com/',
+            icon: iconVk,
+        },
+        {
+            link: 'https://www.instagram.com/',
+            icon: iconInst,
+        },
+        {
+            link: 'https://www.facebook.com/',
+            icon: iconFacebook,
+        },
+    ];
+    const footerSection = document.getElementById('footerWrapper');
 
-  const iconsHTML = icons.map((el) => `<span>${el()}</span>`).join('');
+    const iconsHTML = icons.map((el) => `<a href="${el.link}" target="_blank">${el.icon()}</a>`).join('');
 
-  footerSection.innerHTML = `
+    footerSection.innerHTML = `
     <div class="main-footer__logo">
       ${logo()}
     </div>
@@ -19,8 +33,10 @@ export function renderFooter() {
     </div>
     <div class="main-footer__divider"></div>
     <div class="main-footer__html-academy">
+    <a href="https://htmlacademy.ru/" target="_blank">
       <span>HTML Academy</span>
       <span>${iconHTMLAcademy()}</span>
+      </a>
     </div>
   `;
 }
