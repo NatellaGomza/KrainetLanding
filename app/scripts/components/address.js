8853f2a4c47266bc3564a4a78c65b726ab4679c4
G_GomzaNR <gomza.natella@gmail.com>
2025-11-06 12:11:14 +0300

    fix map icon

--- app/scripts/components/address.js
+++ app/scripts/components/address.js
@@ -1,33 +1,35 @@
 // components/address.scripts
 export function renderMap() {
-  ymaps.ready(() => {
-    const mapContainer = document.getElementById('map');
+    ymaps.ready(() => {
+        const mapContainer = document.getElementById('map');
+        if (!mapContainer) return;
 
-    if (!mapContainer) return;
+        const placemarkCoords = [59.938631, 30.323037];
+        const desktopCenter = [59.939095, 30.315868];
+        const isMobile = window.innerWidth <= 768;
 
-      const coords = [59.938631, 30.323037];
+        const map = new ymaps.Map('map', {
+            center: isMobile ? placemarkCoords : desktopCenter,
+            zoom: 15,
+        });
 
-      const map = new ymaps.Map('map', {
-          center: coords,
-          zoom: 15,
-      });
+        const placemark = new ymaps.Placemark(
+            placemarkCoords,
+            { hintContent: 'Моя точка' },
+            {
+                iconLayout: 'default#image',
+                iconImageHref: './app/assets/map-logo.png',
+                iconImageSize: [48, 48],
+                iconImageOffset: [-24, -48],
+            }
+        );
 
-      const placemark = new ymaps.Placemark(
-          coords,
-          { hintContent: 'Моя точка' },
-          {
-              iconLayout: 'default#image',
-              iconImageHref: './app/assets/map-logo.png',
-              iconImageSize: [48, 48],
-              iconImageOffset: [-24, -48],
-          }
-      );
+        map.geoObjects.add(placemark);
 
-      map.geoObjects.add(placemark);
+        window.addEventListener('resize', () => {
+            const isMobile = window.innerWidth <= 1440;
+            map.setCenter(isMobile ? placemarkCoords : desktopCenter);
+        });
 
-      window.addEventListener('resize', () => {
-          map.setCenter(coords);
-      });
-
-  });
+    });
 }
