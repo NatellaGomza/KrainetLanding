58d3d597ca0fb67d1078010aa7a2f46beaaa30a3
G_GomzaNR <gomza.natella@gmail.com>
2025-11-06 11:52:06 +0300

    fix map center

--- app/scripts/components/address.js
+++ app/scripts/components/address.js
@@ -5,22 +5,29 @@ export function renderMap() {
 
     if (!mapContainer) return;
 
-    const map = new ymaps.Map('map', {
-      center: [59.939095, 30.315868],
-      zoom: 15,
-    });
+      const coords = [59.938631, 30.323037];
 
-    const placemark = new ymaps.Placemark(
-      [59.938631, 30.323037],
-      { hintContent: 'Моя точка' },
-      {
-        iconLayout: 'default#image',
-        iconImageHref: './app/assets/map-logo.png',
-        iconImageSize: [48, 48],
-        iconImageOffset: [-24, -48],
-      }
-    );
+      const map = new ymaps.Map('map', {
+          center: coords,
+          zoom: 15,
+      });
 
-    map.geoObjects.add(placemark);
+      const placemark = new ymaps.Placemark(
+          coords,
+          { hintContent: 'Моя точка' },
+          {
+              iconLayout: 'default#image',
+              iconImageHref: './app/assets/map-logo.png',
+              iconImageSize: [48, 48],
+              iconImageOffset: [-24, -48],
+          }
+      );
+
+      map.geoObjects.add(placemark);
+
+      window.addEventListener('resize', () => {
+          map.setCenter(coords);
+      });
+
   });
 }
